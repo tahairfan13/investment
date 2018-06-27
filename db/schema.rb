46a44697ba698e81d3b0471dcbaf201fcab3d414
Taha Ali Irfan <tahairfan1993@gmail.com>
2018-06-27 14:40:24 +0500

    bootstrap added to registration

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_062227) do
     t.index ["user_id"], name: "index_investments_on_user_id"
   end
 
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
